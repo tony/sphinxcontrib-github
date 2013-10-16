082e4c0a8f6c21caf56b226cb5bf4f9bf6b654eb
Tony Narlock <tony@git-pull.com>
2013-10-17 01:30:08 +0800

    update setup.py, CHANGES

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ else:
 
 setup(
     name='sphinxcontrib-github',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/tony/sphinxcontrib-github/',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-github',
     license='BSD',
